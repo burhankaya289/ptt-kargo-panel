fb548b0518fefc40cffea61c25600b722c02156c
burhankaya289 <burhankaya3400@gmail.com>
2026-06-05 21:11:26 +0300

    Create app.py
    app.py oluşturuldu

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="PTT Kargo Panel",
+    page_icon="📦",
+    layout="wide"
+)
+
+st.title("PTT Kargo Panel")
+
+st.success("Kurulum başarılı")
